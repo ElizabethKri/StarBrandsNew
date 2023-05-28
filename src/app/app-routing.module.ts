6ec39c8482d51b9f22e0d227e203a4e56bed1424
Elizaveta Kritskaya <post.kri@gmail.com>
2023-05-28 22:36:00 +0300

    add app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'starbrands',
+  loadChildren: () => import('./pages/starbrands/starbrands.module').then(m => m.StarbrandsModule)
+  },
+  {path: '**',
+   redirectTo: 'starbrands'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './pages/auth/authorization/authorization.component';
+import { RegistrationComponent } from './pages/auth/registration/registration.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { BasketComponent } from './pages/personal-account/basket/basket.component';
+import { LikeComponent } from './pages/personal-account/like/like.component';
+import { OrdersComponent } from './pages/personal-account/orders/orders.component';
+import { PersonalAccountComponent } from './pages/personal-account/personal-account.component';
+import { StarbrandsComponent } from './pages/starbrands/starbrands.component';
+import {RouterModule} from "@angular/router";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizationComponent,
+    RegistrationComponent,
+    AuthComponent,
+    BasketComponent,
+    LikeComponent,
+    OrdersComponent,
+    PersonalAccountComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
