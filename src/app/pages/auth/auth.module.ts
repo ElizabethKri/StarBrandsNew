ec7b37442633e544437790fc5a98f6318c9cb8c3
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-14 22:58:54 +0300

    add auth

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -2,13 +2,47 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import {RegistrationComponent} from "./registration/registration.component";
+import {AuthorizationComponent} from "./authorization/authorization.component";
+import {AuthComponent} from "./auth.component";
+import {TabViewModule} from "primeng/tabview";
+import {InputTextModule} from "primeng/inputtext";
+import {ToastModule} from "primeng/toast";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CheckboxModule} from "primeng/checkbox";
+import {MessageService} from "primeng/api";
+import {StarbrandsModule} from "../starbrands/starbrands.module";
+import {InputNumberModule} from "primeng/inputnumber";
+import {CalendarModule} from "primeng/calendar";
+import {DropdownModule} from "primeng/dropdown";
+import {InputMaskModule} from "primeng/inputmask";
+import {PasswordModule} from "primeng/password";
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [
+    AuthorizationComponent,
+    AuthComponent,
+    RegistrationComponent],
+
+    imports: [
+        CommonModule,
+      FormsModule,
+      ReactiveFormsModule,
+        AuthRoutingModule,
+        TabViewModule,
+        InputTextModule,
+
+        CheckboxModule,
+        ToastModule,
+        StarbrandsModule,
+        InputNumberModule,
+        CalendarModule,
+        DropdownModule,
+      InputMaskModule,
+      PasswordModule
+    ],
+
+  providers: [MessageService]
 })
 export class AuthModule { }
