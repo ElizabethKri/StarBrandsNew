6905e84c895f2f2dedaca75b4f594d7495910db1
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-14 23:10:04 +0300

    add starbrands

--- src/app/pages/starbrands/starbrands.component.ts
+++ src/app/pages/starbrands/starbrands.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {Products} from "../../interface/products";
+import {ManService} from "../../services/man/man.service";
 
 @Component({
   selector: 'app-starbrands',
   templateUrl: './starbrands.component.html',
   styleUrls: ['./starbrands.component.scss']
 })
-export class StarbrandsComponent {
+export class StarbrandsComponent implements OnInit {
+  products: any[];
+  responsiveOptions: any[];
 
-  constructor() {
+  constructor(private productService: ManService) {
+  }
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then((products) => {
+
+      this.products = products;
+      console.log('this.products', this.products)
+    });
   }
 }
--- src/app/pages/starbrands/starbrands.module.ts
+++ src/app/pages/starbrands/starbrands.module.ts
@@ -8,18 +8,26 @@ import {FooterComponent} from "./footer/footer.component";
 import {StarbrandsListComponent} from "./starbrands-list/starbrands-list.component";
 import {StarbrandsComponent} from "./starbrands.component";
 import {RouterModule} from "@angular/router";
+import {CarouselModule} from "primeng/carousel";
+import {ButtonModule} from "primeng/button";
 
 
 @NgModule({
-  declarations: [HeaderComponent,
-    FooterComponent,
-    StarbrandsListComponent,
-    StarbrandsComponent],
-  imports: [
-    CommonModule,
-    StarbrandsRoutingModule,
-    MenubarModule,
-    RouterModule,
-  ]
+    declarations: [
+        HeaderComponent,
+        FooterComponent,
+        StarbrandsListComponent,
+        StarbrandsComponent],
+    exports: [
+        HeaderComponent
+    ],
+    imports: [
+        CommonModule,
+        StarbrandsRoutingModule,
+        MenubarModule,
+        RouterModule,
+         CarouselModule,
+      ButtonModule
+    ]
 })
 export class StarbrandsModule { }
