f116b7ca721841c711a59f38b095fc8b19c6a831
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-14 20:16:39 +0300

    add auth service

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {IUser} from "../../interface/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+}
