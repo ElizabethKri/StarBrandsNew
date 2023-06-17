7bfb4df0be68473eb25b4c5abcacd2362b0847ad
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-17 21:52:09 +0300

    add starbrands-list

--- src/app/pages/starbrands/starbrands-list/starbrands-list.component.ts
+++ src/app/pages/starbrands/starbrands-list/starbrands-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Products} from "../../../interface/products";
+import {Router} from "@angular/router";
+import {debounceTime, fromEvent, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-starbrands-list',
   templateUrl: './starbrands-list.component.html',
   styleUrls: ['./starbrands-list.component.scss']
 })
-export class StarbrandsListComponent {
+export class StarbrandsListComponent implements OnInit {
+  products: Products[]
 
+
+  constructor(private router: Router,
+  ) { }
+
+  goToProductInfoPage(item: Products){
+    this.router.navigate([`/starbrands/starbrands/${item.productId}`])
+  }
 }
