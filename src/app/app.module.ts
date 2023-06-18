53b5278645ac92137951dfa993d4968b0c63adcf
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-18 23:59:57 +0300

    delete components: Basket, Likes, Orders, Man, Women, Sales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { BasketComponent } from './pages/personal-account/basket/basket.component';
-import { LikeComponent } from './pages/personal-account/like/like.component';
-import { OrdersComponent } from './pages/personal-account/orders/orders.component';
-import { PersonalAccountComponent } from './pages/personal-account/personal-account.component';
+
 import {RouterModule} from "@angular/router";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
-import { ManComponent } from './pages/man/man/man.component';
-import { WomenComponent } from './pages/man/women/women.component';
-import { SalesComponent } from './pages/sales/sales/sales.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasketComponent,
-    LikeComponent,
-    OrdersComponent,
-    PersonalAccountComponent,
-    ManComponent,
-    WomenComponent,
-    SalesComponent
   ],
   imports: [
     BrowserModule,
