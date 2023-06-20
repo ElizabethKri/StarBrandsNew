6011c8ff1438374809857b0b64b75919c8e26137
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-20 19:17:42 +0300

    add starbrands-item

---
+++ src/app/pages/starbrands/starbrands-item/starbrands-item.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarbrandsItemRoutingModule } from './starbrands-item-routing.module';
+import {StarbrandsItemComponent} from "./starbrands-item.component";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StarbrandsItemRoutingModule
+  ]
+})
+export class StarbrandsItemModule { }
