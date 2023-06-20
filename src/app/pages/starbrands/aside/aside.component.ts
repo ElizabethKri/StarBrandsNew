399ebb93838ef40552294be9e5e538f782f95f3e
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-20 22:42:04 +0300

    change aside

--- src/app/pages/starbrands/aside/aside.component.ts
+++ src/app/pages/starbrands/aside/aside.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {IProductTypeSelect} from "../../../interface/products";
-import {WomenService} from "../../../services/women/women.service";
-import {ManService} from "../../../services/man/man.service";
 import {NodeManService} from "../../../services/node/nodeMan.service";
 import {TreeNode} from "primeng/api";
 
