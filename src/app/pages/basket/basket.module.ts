233d7ac7a630fb621e4a2d6e36ce637f47746270
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-20 22:44:13 +0300

    add header + footer

--- src/app/pages/basket/basket.module.ts
+++ src/app/pages/basket/basket.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BasketRoutingModule } from './basket-routing.module';
+import {BasketComponent} from "./basket.component";
+import {StarbrandsModule} from "../starbrands/starbrands.module";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BasketComponent],
   imports: [
     CommonModule,
-    BasketRoutingModule
+    BasketRoutingModule,
+    StarbrandsModule
   ]
 })
 export class BasketModule { }
