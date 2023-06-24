78945244bc8385c6bcd208942d9655a51a3dcb64
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-24 23:27:58 +0300

    delete HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 
 
 import {RouterModule} from "@angular/router";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 
