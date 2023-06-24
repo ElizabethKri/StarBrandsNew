2e3367a2c50e7f37705d03b252ebf16b88dd5701
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-24 23:26:51 +0300

    add pathMatch

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from "./main.component";
 
 const routes: Routes = [
-  {path: '', component: MainComponent}
+  {path: '', component: MainComponent, pathMatch: "full"}
 
 ];
 
