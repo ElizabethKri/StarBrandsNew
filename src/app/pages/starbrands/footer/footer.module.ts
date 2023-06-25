a37aaaffbbb1ec70f8d0d0505d1cf9587b61b749
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-25 14:12:22 +0300

    add free-brands

---
+++ src/app/pages/starbrands/footer/footer.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {BrowserModule} from "@angular/platform-browser";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FontAwesomeModule
+  ]
+})
+export class FooterModule { }
