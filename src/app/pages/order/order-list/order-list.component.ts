41879a8f4fde09b54914bb196face09747f90299
Elizaveta Kritskaya <post.kri@gmail.com>
2023-06-29 22:55:56 +0300

    add sale-item

---
+++ src/app/pages/order/order-list/order-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {BasketService} from "../../../services/basket/basket.service";
+
+@Component({
+  selector: 'app-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.scss']
+})
+export class OrderListComponent implements OnInit{
+  grandTotal: number | string = 0;
+
+  constructor(private router: Router, private basketService: BasketService) {}
+
+  ngOnInit(): void{
+    setTimeout(() => {
+      this.router.navigate(["/starbrands/main"])
+    }, 4000);
+
+    this.grandTotal = this.basketService.getTotalPrice();
+  }
+
+}
---
+++ src/app/pages/sales/sale-item/sale-item.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SaleItemRoutingModule } from './sale-item-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SaleItemRoutingModule
+  ]
+})
+export class SaleItemModule { }
